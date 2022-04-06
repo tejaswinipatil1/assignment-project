0484863b6258f28be97d4021c81bb1b278571ccc
tejaswini patil <tejaswini.patil@mphasis.com>
2022-04-06 16:24:21 +0530

    commit

---
+++ Project5.2/src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+
+	import java.io.*;
+	
+{
+	    public static void main(String args[])
+	    {
+	        String Str = new String("Geeks-for-Geeks");
+	 
+	        // Split above string in at-most two strings 
+	        for (String val: Str.split("-", 2))
+	            System.out.println(val);
+	 
+	        System.out.println("");
+	   
+	        // Splits Str into all possible tokens
+	        for (String val: Str.split("-"))
+	            System.out.println(val);
+	    }
+	}
+}
